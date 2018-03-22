db7f14aa35809c4ca63d6bfde26a707eb48d56c2
phanesh-mallampati <35557714+phanesh-mallampati@users.noreply.github.com>
2018-03-22 20:35:30 +1100

    Initial commit

---
+++ src/test/java/com/phani/services/rest/restfulservices/RestfulServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.phani.services.rest.restfulservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
